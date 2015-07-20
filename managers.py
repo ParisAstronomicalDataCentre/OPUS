0bd5d2212e3811683cc103125aad31b750c804c1
Mathieu Servillat <mathieu@servillat.com>
2015-07-20 15:45:39 +0200

    update PBS

--- managers.py
+++ managers.py
@@ -97,7 +97,10 @@ class SLURMManager(Manager):
             #'/obs/vouws/uws_scripts/ctbin.pl 'voplus.obspm.fr/cta/events.fits' 5',
             # Init job execution
             'echo "Set die and trap"',
-            'function error_handler { echo "${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: ${FUNCNAME[1]}"; exit 1 }',
+            'function error_handler {',
+            '    echo "${BASH_SOURCE[1]}: line ${BASH_LINENO[0]}: ${FUNCNAME[1]}"',
+            '    exit 1',
+            '}',
             'trap error_handler ERR',
             'wd={}{}'.format(self.working_path, job.jobid),
             'rd={}{}'.format(self.results_path, job.jobid),
