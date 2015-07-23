500497ccb75b270023c753e0b64ad09ee996df45
Mathieu Servillat <mathieu@servillat.com>
2015-07-23 17:34:12 +0200

    scp results to uws-test

--- uws_server.py
+++ uws_server.py
@@ -856,7 +856,10 @@ def get_result(jobname, jobid, rname):
         logger.debug(fname)
         logger.debug(fname)
         response.set_header('Content-type', job.results[rname]['mediaType'])
-        return static_file(fname, root='{}/{}'.format(DATA_PATH, job.jobid), download=fname)
+        if 'text' in job.results[rname]['mediaType']:
+            return static_file(fname, root='{}/{}'.format(DATA_PATH, job.jobid))
+        else:
+            return static_file(fname, root='{}/{}'.format(DATA_PATH, job.jobid), download=fname)
     except storage.NotFoundWarning as e:
         abort_404(e.message)
     except:
