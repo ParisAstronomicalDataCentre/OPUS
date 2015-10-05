de4eaf7613fd3f65bd3e09b0e7a4dc484759f3d3
Mathieu Servillat <mathieu@servillat.com>
2015-10-05 15:52:18 +0200

    add timestamp to sbatch logs

--- managers.py
+++ managers.py
@@ -107,6 +107,9 @@ class SLURMManager(Manager):
             'echo "Working dir is $wd"',
             'echo "Current dir is $pwd"',
             'echo "JobData dir is $jd"',
+            'timestamp() {',
+            '    date +"%Y-%m-%dT%H:%M:%S"',
+            '}',
             'echo "Set trap"',
             'set -e ',
             # Error handler (send signal on error, in addition to job completion by SLURM)
@@ -130,7 +133,7 @@ class SLURMManager(Manager):
             'curl -s -o $jd/curl_start_signal.log '
             '    -d "jobid=$SLURM_JOBID" -d "phase=RUNNING" '
             '    {}/handler/job_event'.format(BASE_URL),
-            'echo "Start job"',
+            'echo timestamp "Start job"',
             'touch $jd/start',
             '### EXEC',
             # Load variables from params file
@@ -151,7 +154,7 @@ class SLURMManager(Manager):
             '### CLEAN',
             'rm -rf $wd',
             'touch $jd/done',
-            'echo "Job done"',
+            'echo timestamp "Job done"',
             'echo "Remove trap"',
             'trap - INT TERM EXIT',
             'exit 0',
