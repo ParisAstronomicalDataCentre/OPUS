3188a3798b3e504edbc4080af861a76b0023d8d9
mservillat <mathieu.servillat@obspm.fr>
2015-10-29 17:14:59 +0100

    change use of working directory: need to be created when job start on a node as it is visible to this node only

--- scripts/serpe.sh
+++ scripts/serpe.sh
@@ -2,9 +2,11 @@
 # Utilisation des jetons IDL du LESIA
 IDL_LMGRD_LICENSE_FILE=1700@jetons-lesia.obspm.fr:1700@jetons.obspm.fr
 # Load IDL libs
-SERPE_FRTEND_PATH='/obs/vouws/serpe'
-SERPE_DATA_DIR='/obs/vouws/serpe/data'
-SERPE_RESULT_DIR=$rd
-#config='Io_21_01_2013.srp'
+SERPE_FRTEND_PATH='/home/vouws/serpe'
+SERPE_DATA_DIR='/home/vouws/serpe/data'
+SERPE_RESULT_DIR=$wd
+echo ${config}
 idlcmd="restore, '${SERPE_FRTEND_PATH}/SERPE.sav' & main"
+echo ${idlcmd}
+# Run IDL command
 echo ${config} | idl -e "$idlcmd"
