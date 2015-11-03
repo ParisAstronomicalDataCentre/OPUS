afee1543aae265a2745ba99c7a579e1c02ed90d2
mservillat <mathieu.servillat@obspm.fr>
2015-11-03 11:19:48 +0100

    show form input

--- static/js/uws_manager.js
+++ static/js/uws_manager.js
@@ -395,12 +395,12 @@ var uws_manager = (function($) {
         for (var pname in jdl.parameters) {
             var p = jdl.parameters[pname];
             displayParamFormInput(pname, p);
-            $('#id_'+pname).wrap('<div class="input-group"></div>');
-            // Signal default value
-            if (!(pname in job['parameters'])) {
-                $('#id_'+pname).parent().append('<span class="input-group-addon" style="line-height: 1.4;"><small>default used</small></span>');
-            };
             if (p.type != 'file') {
+                $('#id_'+pname).wrap('<div class="input-group"></div>');
+                // Signal default value
+                if (!(pname in job['parameters'])) {
+                    $('#id_'+pname).parent().append('<span class="input-group-addon" style="line-height: 1.4;"><small>default used</small></span>');
+                };
                 // Add Update buttons (possible to update params when pĥase is PENDING in UWS 1.0 - but not yet implemented)
                 $('#id_'+pname).parent().append('<span class="input-group-btn"><button id="button_'+pname+'" class="btn btn-default" type="button">Update</button></span>');
                 // Change input type
