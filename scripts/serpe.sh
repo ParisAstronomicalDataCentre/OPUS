a98e48f109b7807d476026b089821107cb21e4b6
mservillat <mathieu.servillat@obspm.fr>
2015-11-12 15:36:04 +0100

    add gammastart

--- scripts/serpe.sh
+++ scripts/serpe.sh
@@ -4,7 +4,7 @@ export IDL_LMGRD_LICENSE_FILE=1700@jetons-lesia.obspm.fr:1700@jetons.obspm.fr
 # Load IDL libs
 export SERPE_DATA_DIR=/data/vouws/serpe/
 echo "config=${config}"
-idlcmd='restore, "/obs/vouws/serpe/SERPE.sav" & main'
+idlcmd='restore, "/obs/vouws/scripts/serpe/SERPE.sav" & main'
 echo ${idlcmd}
 # Run IDL command
 echo ${config} | idl -e "$idlcmd"
