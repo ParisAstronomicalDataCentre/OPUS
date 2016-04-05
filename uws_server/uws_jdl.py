09c949ea521aef8d41547d158ee770ad05bd7439
mservillat <mathieu.servillat@obspm.fr>
2016-04-05 16:22:46 +0200

    clean uws_jdl.py

--- uws_server/uws_jdl.py
+++ uws_server/uws_jdl.py
@@ -207,139 +207,139 @@ class WADLFile(JDLFile):
 
 
 
-
-
-# xmlns = '{http://wadl.dev.java.net/2009/02}'
-xmlns_uris = {'xmlns:wadl': 'http://wadl.dev.java.net/2009/02',
-              'xmlns:uws': 'http://www.ivoa.net/xml/UWS/v1.0',
-              'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-              'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
-              'xsi:schemaLocation': 'http://wadl.dev.java.net/2009/02 http://www.w3.org/Submission/wadl/wadl.xsd'}
-
-
-def read_jdl(jobname):
-    """Read job description from WADL file"""
-    fname = '{}/{}.wadl'.format(JDL_PATH, jobname)
-    parameters = collections.OrderedDict()
-    results = collections.OrderedDict()
-    try:
-        with open(fname, 'r') as f:
-            wadl_string = f.read()
-        wadl_tree = ETree.fromstring(wadl_string)
-        # Get default namespace
-        xmlns = '{' + wadl_tree.nsmap[None] + '}'
-        # Read parameters description
-        params_block = wadl_tree.find(".//{}request[@id='create_job_parameters']".format(xmlns))
-        for p in params_block.getchildren():
-            pname = p.get('name')
-            if pname not in ['PHASE', None]:
-                # TODO: Add all attributes (e.g. min, max for numbers)
-                parameters[pname] = {
-                    'type': p.get('type'),
-                    'required': p.get('required'),
-                    'default': p.get('default'),
-                    'description': list(p)[0].text,
-                }
-                for attr in ['min', 'max', 'choices']:
-                    if p.get(attr):
-                        parameters[pname][attr] = p.get(attr)
-        # Read results description
-        results_block = wadl_tree.find(".//{}param[@name='result-id']".format(xmlns))
-        for r in results_block.getchildren():
-            if r.get('value') not in [None]:
-                results[r.get('value')] = {
-                    'mediaType': r.get('mediaType'),
-                    'default': r.get('default'),
-                    'description': list(r)[0].text,
-                }
-        # Read execution duration
-        execdur_block = wadl_tree.find(".//{}param[@name='EXECUTIONDURATION']".format(xmlns))
-        execdur = execdur_block.get('default')
-        # Read default quote
-        quote_block = wadl_tree.find(".//{}representation[@id='quote']".format(xmlns))
-        quote = quote_block.get('default')
-        # Read job description
-        job_list_description_block = wadl_tree.find(".//{}doc[@title='description']".format(xmlns))
-        description = job_list_description_block.text
-        # Log wadl access
-        frame, filename, line_number, function_name, lines, index = inspect.stack()[1]
-        logger.debug('WADL read at {} ({}:{}): {}'.format(function_name, filename, line_number, fname))
-    except IOError:
-        # if file does not exist, continue and return an empty dict
-        logger.debug('WADL not found for job {}'.format(jobname))
-        raise UserWarning('WADL not found for job {}'.format(jobname))
-        # return {}
-    job_def = {'description': description,
-               'parameters': parameters,
-               'results': results,
-               'executionduration': execdur,
-               'quote': quote}
-    return job_def
-
-
-def create_jdl(jobname, job_def):
-    """Open WADL template and insert job definition"""
-    jobname = jobname.split('/')[-1]
-    # Prepare parameter blocks
-    wadl_params = []
-    wadl_popts = []
-    for pname, p in job_def['parameters'].iteritems():
-        # pline = '<param style="query" name="{}" type="{}" required="{}" default="{}"><doc>{}</doc></param>' \
-        #        ''.format(pname, p['type'], p['required'], p['default'], p.get('description', ''))
-        pelt_attrib = {'style': 'query',
-                       'name': pname,
-                       'type': p['type'],
-                       'required': str(p['required']),
-                       'default': p['default']}
-        pelt = ETree.Element('param', attrib=pelt_attrib)
-        ETree.SubElement(pelt, 'doc').text = p.get('description', '')
-        wadl_params.append(pelt)
-        # line = '<option value="{}" mediaType="text/plain"><doc>{}</doc></option>' \
-        #        ''.format(pname, p['description'])
-        poelt = ETree.Element('option', attrib={'value': pname, 'mediaType': 'text/plain'})
-        ETree.SubElement(poelt, 'doc').text = p.get('description', '')
-        wadl_popts.append(poelt)
-    # Prepare result block
-    wadl_ropts = []
-    for rname, r in job_def['results'].iteritems():
-        # rline = '<option value="{}" mediaType="{}" default="{}"><doc>{}</doc></option>' \
-        #         ''.format(rname, r['mediaType'], r['default'], r.get('description', ''))
-        roelt = ETree.Element('option', attrib={'value': rname, 'mediaType': r['mediaType'], 'default': r['default']})
-        ETree.SubElement(roelt, 'doc').text = r.get('description', '')
-        wadl_ropts.append(roelt)
-    # Read WADL UWS template as XML Tree
-    filename = '{}/uws_template.wadl'.format(JDL_PATH)
-    with open(filename, 'r') as f:
-        wadl_string = f.read()
-    wadl_tree = ETree.fromstring(wadl_string)
-    xmlns = '{' + wadl_tree.nsmap[None] + '}'
-    # Insert jobname
-    joblist_block = wadl_tree.find(".//{}resource[@id='joblist']".format(xmlns))
-    joblist_block.set('path', jobname)
-    # Insert job description
-    job_list_description_block = wadl_tree.find(".//{}doc[@title='description']".format(xmlns))
-    job_list_description_block.text = job_def['description'].decode()
-    # Insert parameters
-    params_block = {}
-    for block in ['create_job_parameters', 'control_job_parameters', 'set_job_parameters']:
-        params_block[block] = wadl_tree.find(".//{}request[@id='{}']".format(xmlns, block))
-        for pelt in wadl_params:
-            params_block[block].append(copy.copy(pelt))
-    # Insert parameters as options
-    param_opts_block = wadl_tree.find(".//{}param[@name='parameter-name']".format(xmlns))
-    for poelt in wadl_popts:
-        param_opts_block.append(poelt)
-    # Insert results as options
-    result_opts_block = wadl_tree.find(".//{}param[@name='result-id']".format(xmlns))
-    for roelt in wadl_ropts:
-        result_opts_block.append(roelt)
-    # Insert default execution duration
-    execdur_block = wadl_tree.find(".//{}param[@name='EXECUTIONDURATION']".format(xmlns))
-    execdur_block.set('default', job_def['executionduration'])
-    # Insert default quote
-    quote_block = wadl_tree.find(".//{}representation[@id='quote']".format(xmlns))
-    quote_block.set('default', job_def['quote'])
-    return ETree.tostring(wadl_tree, pretty_print=True)
+#
+#
+# # xmlns = '{http://wadl.dev.java.net/2009/02}'
+# xmlns_uris = {'xmlns:wadl': 'http://wadl.dev.java.net/2009/02',
+#               'xmlns:uws': 'http://www.ivoa.net/xml/UWS/v1.0',
+#               'xmlns:xlink': 'http://www.w3.org/1999/xlink',
+#               'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+#               'xsi:schemaLocation': 'http://wadl.dev.java.net/2009/02 http://www.w3.org/Submission/wadl/wadl.xsd'}
+#
+#
+# def read_jdl(jobname):
+#     """Read job description from WADL file"""
+#     fname = '{}/{}.wadl'.format(JDL_PATH, jobname)
+#     parameters = collections.OrderedDict()
+#     results = collections.OrderedDict()
+#     try:
+#         with open(fname, 'r') as f:
+#             wadl_string = f.read()
+#         wadl_tree = ETree.fromstring(wadl_string)
+#         # Get default namespace
+#         xmlns = '{' + wadl_tree.nsmap[None] + '}'
+#         # Read parameters description
+#         params_block = wadl_tree.find(".//{}request[@id='create_job_parameters']".format(xmlns))
+#         for p in params_block.getchildren():
+#             pname = p.get('name')
+#             if pname not in ['PHASE', None]:
+#                 # TODO: Add all attributes (e.g. min, max for numbers)
+#                 parameters[pname] = {
+#                     'type': p.get('type'),
+#                     'required': p.get('required'),
+#                     'default': p.get('default'),
+#                     'description': list(p)[0].text,
+#                 }
+#                 for attr in ['min', 'max', 'choices']:
+#                     if p.get(attr):
+#                         parameters[pname][attr] = p.get(attr)
+#         # Read results description
+#         results_block = wadl_tree.find(".//{}param[@name='result-id']".format(xmlns))
+#         for r in results_block.getchildren():
+#             if r.get('value') not in [None]:
+#                 results[r.get('value')] = {
+#                     'mediaType': r.get('mediaType'),
+#                     'default': r.get('default'),
+#                     'description': list(r)[0].text,
+#                 }
+#         # Read execution duration
+#         execdur_block = wadl_tree.find(".//{}param[@name='EXECUTIONDURATION']".format(xmlns))
+#         execdur = execdur_block.get('default')
+#         # Read default quote
+#         quote_block = wadl_tree.find(".//{}representation[@id='quote']".format(xmlns))
+#         quote = quote_block.get('default')
+#         # Read job description
+#         job_list_description_block = wadl_tree.find(".//{}doc[@title='description']".format(xmlns))
+#         description = job_list_description_block.text
+#         # Log wadl access
+#         frame, filename, line_number, function_name, lines, index = inspect.stack()[1]
+#         logger.debug('WADL read at {} ({}:{}): {}'.format(function_name, filename, line_number, fname))
+#     except IOError:
+#         # if file does not exist, continue and return an empty dict
+#         logger.debug('WADL not found for job {}'.format(jobname))
+#         raise UserWarning('WADL not found for job {}'.format(jobname))
+#         # return {}
+#     job_def = {'description': description,
+#                'parameters': parameters,
+#                'results': results,
+#                'executionduration': execdur,
+#                'quote': quote}
+#     return job_def
+#
+#
+# def create_jdl(jobname, job_def):
+#     """Open WADL template and insert job definition"""
+#     jobname = jobname.split('/')[-1]
+#     # Prepare parameter blocks
+#     wadl_params = []
+#     wadl_popts = []
+#     for pname, p in job_def['parameters'].iteritems():
+#         # pline = '<param style="query" name="{}" type="{}" required="{}" default="{}"><doc>{}</doc></param>' \
+#         #        ''.format(pname, p['type'], p['required'], p['default'], p.get('description', ''))
+#         pelt_attrib = {'style': 'query',
+#                        'name': pname,
+#                        'type': p['type'],
+#                        'required': str(p['required']),
+#                        'default': p['default']}
+#         pelt = ETree.Element('param', attrib=pelt_attrib)
+#         ETree.SubElement(pelt, 'doc').text = p.get('description', '')
+#         wadl_params.append(pelt)
+#         # line = '<option value="{}" mediaType="text/plain"><doc>{}</doc></option>' \
+#         #        ''.format(pname, p['description'])
+#         poelt = ETree.Element('option', attrib={'value': pname, 'mediaType': 'text/plain'})
+#         ETree.SubElement(poelt, 'doc').text = p.get('description', '')
+#         wadl_popts.append(poelt)
+#     # Prepare result block
+#     wadl_ropts = []
+#     for rname, r in job_def['results'].iteritems():
+#         # rline = '<option value="{}" mediaType="{}" default="{}"><doc>{}</doc></option>' \
+#         #         ''.format(rname, r['mediaType'], r['default'], r.get('description', ''))
+#         roelt = ETree.Element('option', attrib={'value': rname, 'mediaType': r['mediaType'], 'default': r['default']})
+#         ETree.SubElement(roelt, 'doc').text = r.get('description', '')
+#         wadl_ropts.append(roelt)
+#     # Read WADL UWS template as XML Tree
+#     filename = '{}/uws_template.wadl'.format(JDL_PATH)
+#     with open(filename, 'r') as f:
+#         wadl_string = f.read()
+#     wadl_tree = ETree.fromstring(wadl_string)
+#     xmlns = '{' + wadl_tree.nsmap[None] + '}'
+#     # Insert jobname
+#     joblist_block = wadl_tree.find(".//{}resource[@id='joblist']".format(xmlns))
+#     joblist_block.set('path', jobname)
+#     # Insert job description
+#     job_list_description_block = wadl_tree.find(".//{}doc[@title='description']".format(xmlns))
+#     job_list_description_block.text = job_def['description'].decode()
+#     # Insert parameters
+#     params_block = {}
+#     for block in ['create_job_parameters', 'control_job_parameters', 'set_job_parameters']:
+#         params_block[block] = wadl_tree.find(".//{}request[@id='{}']".format(xmlns, block))
+#         for pelt in wadl_params:
+#             params_block[block].append(copy.copy(pelt))
+#     # Insert parameters as options
+#     param_opts_block = wadl_tree.find(".//{}param[@name='parameter-name']".format(xmlns))
+#     for poelt in wadl_popts:
+#         param_opts_block.append(poelt)
+#     # Insert results as options
+#     result_opts_block = wadl_tree.find(".//{}param[@name='result-id']".format(xmlns))
+#     for roelt in wadl_ropts:
+#         result_opts_block.append(roelt)
+#     # Insert default execution duration
+#     execdur_block = wadl_tree.find(".//{}param[@name='EXECUTIONDURATION']".format(xmlns))
+#     execdur_block.set('default', job_def['executionduration'])
+#     # Insert default quote
+#     quote_block = wadl_tree.find(".//{}representation[@id='quote']".format(xmlns))
+#     quote_block.set('default', job_def['quote'])
+#     return ETree.tostring(wadl_tree, pretty_print=True)
 
 
 
