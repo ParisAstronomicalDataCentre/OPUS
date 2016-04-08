2464d2e4b2f4b4044d20d302447067afb2f78874
mservillat <mathieu.servillat@obspm.fr>
2016-04-08 22:49:42 +0200

    add wsgi.py for uws_client

---
+++ uws_client/wsgi.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2016 by Mathieu Servillat
+# Licensed under MIT (https://github.com/mservillat/uws-server/blob/master/LICENSE)
+"""
+WSGI script for UWS server
+"""
+
+import os
+import sys
+
+curdir = os.path.dirname(__file__)
+sys.path.append(curdir)
+
+# Change working directory so relative paths (and template lookup) work again
+os.chdir(curdir)
+
+import bottle
+from uws_client import uws_client
+
+# Do NOT use bottle.run() with mod_wsgi
+application = uws_client.app
