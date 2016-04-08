e710e81914419dabc3a0325eda8b6c5c58c3fccd
mservillat <mathieu.servillat@obspm.fr>
2016-04-08 15:48:46 +0200

    clean code

--- uws_client/static/js/job_definition.js
+++ uws_client/static/js/job_definition.js
@@ -7,27 +7,6 @@
 	"use strict";
 
     var editor
-    var param_type_options = [
-        "xs:string",
-        "xs:anyURI",
-        "file",
-        "xs:float",
-        "xs:double",
-        "xs:int",
-        "xs:long",
-        "xs:boolean",
-    ];
-    var result_type_options = [
-        "text/plain",
-        "text/xml",
-        "text/x-votable+xml",
-        "application/json",
-        "application/pdf",
-        "image/jpeg",
-        "image/png",
-        "image/fits",
-        "video/mp4",
-    ];
 
     var type_options = {
         'param': [
@@ -193,261 +172,24 @@
 	}
 
 	function move_item_down(type, ii) {
-	    var $node = $('#param_' + ii)
+	    var $node = $('#' + type + '_' + ii)
         $node.next().after($node);
         reset_item_numbers(type);
 	}
-
-	function remove_all_items(type) {
-	    var list_table = $('#' + type + '_list tbody');
-	    list_table.children().remove();
-    }
-
-	function remove_last_parameter(type) {
-	    var list_table = $('#' + type + '_list tbody');
-	    var ii = list_table.children().length;
-	    if (ii > 0) {
-    	    $('#' + type + '_' + ii).remove();
-	    }
-    }
 
 	function remove_parameter(type, ii) {
         $('#' + type + '_' + ii).remove();
         reset_item_numbers(type);
     }
 
-    // ----------
-    // Parameters
-
-	function add_parameter() {
-	    var mytable = $("#parameter_list tbody");
-	    var iparam = mytable.children().length + 1;
-	    var options = '<option>' + param_type_options.join('</option><option>') + '</option>';
-        var row = '\
-            <tr id="param_' + iparam + '">\
-                <td>\
-                    <div class="input-group input-group-sm col-md-12">\
-                        <span class="input-group-btn">\
-                            <button id="remove_param_' + iparam + '" class="remove_param btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;" >\
-                                <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>\
-                            </button>\
-                            <button id="moveup_param_' + iparam + '" class="moveup_param btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;" >\
-                                <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>\
-                            </button>\
-                            <button id="movedown_param_' + iparam + '" class="movedown_param btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;" >\
-                                <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>\
-                            </button>\
-                        </span>\
-                        <input class="param_name form-control" style="font-weight: bold;" name="param_name_' + iparam + '" type="text" placeholder="Name" />\
-                        <span class="input-group-addon">=</span>\
-                        <input class="param_default form-control" name="param_default_' + iparam + '" type="text" placeholder="Default value" />\
-                        <span class="input-group-addon">\
-                            <input class="param_required" name="param_required_' + iparam + '" type="checkbox" title="Required parameter?" checked/>\
-                        </span>\
-                        <span class="input-group-btn">\
-                            <select class="param_type select-small selectpicker" name="param_type_' + iparam + '">\
-                                ' + options + '\
-                            </select>\
-                        </span>\
-                    </div>\
-                    <div style="height: 1px;"></div>\
-                    <div class="input-group input-group-sm col-md-12">\
-                        <input class="param_description form-control" name="param_description_' + iparam + '" type="text" placeholder="Description" style="border-radius: 4px;" />\
-                    </div>\
-                    <div style="height: 8px;"></div>\
-                </td>\
-            </tr>';
-        // Append row
-        $('#parameter_list > tbody').append($(row));
-        // Initialize selectpicker
-        $(".selectpicker").attr('data-width', '140px').selectpicker();
-        // Add click events for remove/moveup/movedown
-        $('#remove_param_' + iparam).click( function() {
-            iparam = this.id.split('_').pop();
-            remove_parameter(iparam);
-        });
-        $('#moveup_param_' + iparam).click( function() {
-            iparam = this.id.split('_').pop();
-            move_parameter_up(iparam);
-        });
-        $('#movedown_param_' + iparam).click( function() {
-            iparam = this.id.split('_').pop();
-            move_parameter_down(iparam);
-        });
-	}
-
-	function reset_parameter_numbers(parent) {
-        parent.children().each( function(i) {
-            i++;
-            if ($(this).attr('id') != 'param_' + i) {
-                $(this).find('.remove_param').attr('id', 'remove_param_' + i);
-                $(this).find('.moveup_param').attr('id', 'moveup_param_' + i);
-                $(this).find('.movedown_param').attr('id', 'movedown_param_' + i);
-                $(this).find('.param_name').attr('name', 'param_name_' + i);
-                $(this).find('.param_default').attr('name', 'param_default_' + i);
-                $(this).find('.param_required').attr('name', 'param_required_' + i);
-                $(this).find('.param_type').attr('name', 'param_type_' + i);
-                $(this).find('.param_description').attr('name', 'param_description_' + i);
-                $(this).attr('id', 'param_' + i);
-            }
-        });
-        $('.selectpicker').selectpicker('refresh');
-	}
-
-	function move_parameter_up(iparam) {
-	    var $node = $('#param_' + iparam)
-        $node.prev().before($node);
-        reset_parameter_numbers($node.parent())
-	}
-
-	function move_parameter_down(iparam) {
-	    var $node = $('#param_' + iparam)
-        $node.next().after($node);
-        reset_parameter_numbers($node.parent())
-	}
-
-	function remove_all_parameters() {
-	    var mytable = $("#parameter_list tbody");
-	    var nparam = mytable.children().length;
-	    for (var i = nparam; i > 0; i--) {
-    	    $('#param_' + i).remove();
-	    }
+	function remove_last_parameter(type) {
+	    var list_table = $('#' + type + '_list tbody');
+	    list_table.children().last().remove();
     }
 
-	function remove_last_parameter() {
-	    var mytable = $("#parameter_list tbody");
-	    var iparam = mytable.children().length;
-	    if (iparam > 0) {
-    	    $('#param_' + iparam).remove();
-	    }
-    }
-
-	function remove_parameter(iparam) {
-	    var mytable = $("#parameter_list tbody");
-	    var nparam = mytable.children().length;
-        $('#param_' + iparam).remove();
-        reset_parameter_numbers($node.parent())
-    }
-
-    // ----------
-    // Results
-
-	function add_result() {
-	    var mytable = $("#result_list tbody");
-	    var iresult = mytable.children().length + 1;
-	    var options = '<option>' + result_type_options.join('</option><option>') + '</option>';
-        var row = '\
-            <tr id="result_' + iresult + '">\
-                <td>\
-                    <div class="input-group input-group-sm col-md-12">\
-                        <span class="input-group-btn">\
-                            <button id="remove_result_' + iresult + '" class="remove_result btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;">\
-                                <span class="glyphicon glyphicon-remove"></span>\
-                            </button>\
-                            <button id="moveup_result_' + iresult + '" class="moveup_result btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;" >\
-                                <span class="glyphicon glyphicon-arrow-up" aria-hidden="true"></span>\
-                            </button>\
-                            <button id="movedown_result_' + iresult + '" class="movedown_result btn btn-default" type="button" style="border-bottom-left-radius: 4px; border-top-left-radius: 4px;" >\
-                                <span class="glyphicon glyphicon-arrow-down" aria-hidden="true"></span>\
-                            </button>\
-                        </span>\
-                        <input class="result_name form-control" style="font-weight: bold;" name="result_name_' + iresult + '" type="text" placeholder="Name" />\
-                        <span class="input-group-addon">=</span>\
-                        <input class="result_default form-control" name="result_default_' + iresult + '" type="text" placeholder="Default value" />\
-                        <span class="input-group-btn">\
-                            <select name="result_type_' + iresult + '" class="result_type select-small selectpicker">\
-                                ' + options + '\
-                            </select>\
-                        </span>\
-                    </div>\
-                    <div style="height: 1px;"></div>\
-                    <div class="input-group input-group-sm col-md-12">\
-                        <input class="result_description form-control" name="result_description_' + iresult + '" type="text" placeholder="Description" style="border-radius: 4px;" />\
-                    </div>\
-                    <div style="height: 8px;"></div>\
-                </td>\
-            </tr>';
-        // Append row
-        $('#result_list > tbody').append($(row));
-        // Initialize selectpicker
-        $(".selectpicker").attr('data-width', '140px').selectpicker();
-        // Add click events for remove/moveup/movedown
-        $('#remove_result_' + iresult).click( function() {
-            iresult = this.id.split('_').pop();
-            remove_result(iresult);
-        });
-        $('#moveup_result_' + iresult).click( function() {
-            iresult = this.id.split('_').pop();
-            move_result_up(iresult);
-        });
-        $('#movedown_result_' + iresult).click( function() {
-            iresult = this.id.split('_').pop();
-            move_result_down(iresult);
-        });
-	}
-
-	function reset_result_numbers(parent) {
-        parent.children().each( function(i) {
-            i++;
-            if ($(this).attr('id') != 'result_' + i) {
-                console.log('renumber results');
-                $(this).find('.remove_result').attr('id', 'remove_result_' + i);
-                $(this).find('.moveup_result').attr('id', 'moveup_result_' + i);
-                $(this).find('.movedown_result').attr('id', 'movedown_result_' + i);
-                $(this).find('.result_name').attr('name', 'result_name_' + i);
-                $(this).find('.result_default').attr('name', 'result_default_' + i);
-                $(this).find('.result_type').attr('name', 'result_type_' + i);
-                $(this).find('.result_description').attr('name', 'result_description_' + i);
-                $(this).attr('id', 'result_' + i);
-            }
-        });
-        $('.selectpicker').selectpicker('refresh');
-	}
-
-	function move_result_up(iresult) {
-	    var $node = $('#result_' + iresult)
-        $node.prev().before($node);
-        reset_result_numbers($node.parent())
-	}
-
-	function move_result_down(iresult) {
-	    var $node = $('#result_' + iresult)
-        $node.next().after($node);
-        reset_result_numbers($node.parent())
-	}
-
-
-	function remove_all_results() {
-	    var mytable = $("#result_list tbody");
-	    var nresult = mytable.children().length;
-	    for (var i = nresult; i > 0; i--) {
-            $('#result_' + i).remove();
-	    }
-    }
-
-	function remove_last_result() {
-	    var mytable = $("#result_list tbody");
-	    var iresult = mytable.children().length;
-	    if (iresult > 0) {
-	        $('#result_' + iresult).remove();
-	    }
-    }
-
-	function remove_result(iresult) {
-	    var mytable = $("#result_list tbody");
-	    var nresult = mytable.children().length;
-        $('#result_' + iresult).remove();
-	    for (var i = Number(iresult); i < nresult; i++) {
-	        var j = i + 1;
-    	    console.log(j + '->' + i);
-    	    console.log($('#result_' + j).prop('id'));
-    	    $('#result_' + j).prop('id', 'result_' + i);
-    	    $('#remove_result_' + j).prop('id', 'remove_result_' + i);
-    	    $('input[name=result_name_' + j + ']').attr('name', 'result_name_' + i);
-    	    $('input[name=result_default_' + j + ']').attr('name', 'result_default_' + i);
-    	    $('select[name=result_type_' + j + ']').attr('name', 'result_type_' + i);
-    	    $('input[name=result_description_' + j + ']').attr('name', 'result_description_' + i);
-	    }
+	function remove_all_items(type) {
+	    var list_table = $('#' + type + '_list tbody');
+	    list_table.children().remove();
     }
 
     // ----------
@@ -569,11 +311,12 @@
             console.log('no jobname given');
         });
         $('#add_parameter').click( function() { add_item('param'); });
-        $('#remove_last_parameter').click( function() { remove_last_item('param'); });
+        // $('#remove_last_parameter').click( function() { remove_last_item('param'); });
         $('#remove_all_parameters').click( function() { remove_all_items('param'); });
         $('#add_result').click( function() { add_item('result'); });
-        $('#remove_last_result').click( function() { remove_last_item('result'); });
+        // $('#remove_last_result').click( function() { remove_last_item('result'); });
         $('#remove_all_results').click( function() { remove_all_items('result'); });
+        // Load JDL on return keydown for job name
         $('input[name=name]').keydown(function (event) {
             if (event.keyCode == 13) {
                 event.preventDefault();
