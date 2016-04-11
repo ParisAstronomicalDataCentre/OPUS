29741435fdb860777efa9299cdf2e0fa49ff4a8b
mservillat <mathieu.servillat@obspm.fr>
2016-04-11 17:19:05 +0200

    get_jobnames

--- uws_client/static/js/job_list.js
+++ uws_client/static/js/job_list.js
@@ -16,7 +16,7 @@
             type : 'GET',
             dataType: "json",
             success : function(json) {
-                for each (jn in json['jobnames']) {
+                for each (var jn in json['jobnames']) {
                     $('.selectpicker').append('<option>' + jn + '</option>')
                 };
             $('.selectpicker').selectpicker('refresh');
