bbdae28b7fb37e73b6c1622dc8c7e9ad255ae46f
mservillat <mathieu.servillat@obspm.fr>
2016-05-03 18:04:08 +0200

    typos

--- uws_client/static/js/uws_manager.js
+++ uws_client/static/js/uws_manager.js
@@ -378,7 +378,7 @@ var uws_manager = (function($) {
         };
     };
     var displayParamFormOk = function(jobName){
-        // Run displayParamForm insread to check that jdl is defined
+        // Run displayParamForm instead to check that jdl is defined
         var jdl = clients[jobName].jdl;
         // Create form fields from WADL/JDL
         for (var pname in jdl.parameters) {
