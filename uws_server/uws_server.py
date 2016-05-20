1365d1479531eb46762924b90d898fb6a0b734dc
mservillat <mathieu.servillat@obspm.fr>
2016-05-20 11:44:34 +0200

    maintenance code

--- uws_server/uws_server.py
+++ uws_server/uws_server.py
@@ -530,7 +530,7 @@ def maintenance(jobname):
         abort_500_except()
     # Response
     response.content_type = 'text/plain; charset=UTF-8'
-    return ''
+    return 'Maintenance performed'
 
 
 # ----------
