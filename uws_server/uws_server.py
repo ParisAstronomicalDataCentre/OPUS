b6df2ce1ecc6903195abf7bbec1bd8d6f3a2907f
mservillat <mathieu.servillat@obspm.fr>
2016-05-20 13:51:44 +0200

    add try to get_result_file

--- uws_server/uws_server.py
+++ uws_server/uws_server.py
@@ -1159,23 +1159,28 @@ def get_result_file(jobid, rname, rfname):
         404 Not Found: Result not found (on NotFoundWarning)
         500 Internal Server Error (on error)
     """
-    user = set_user()
-    # Get job properties from DB
-    job = Job('', jobid, user, get_parameters=True, get_results=True)
-    # Check if result exists
-    if rname not in job.results:
-        raise storage.NotFoundWarning('Result "{}" NOT FOUND for job "{}"'.format(rname, jobid))
-    # Return result
-    # rfname = job.get_result_filename(rname)
-    #response.content_type = 'text/plain; charset=UTF-8'
-    #return str(job.results[result]['url'])
-    media_type = job.results[rname]['mediaType']
-    logger.debug('{} {} {} {} {}'.format(job.jobname, jobid, rname, rfname, media_type))
-    response.set_header('Content-type', media_type)
-    if any(x in media_type for x in ['text', 'xml', 'json']):
-        return static_file(rfname, root='{}/{}/results'.format(JOBDATA_PATH, job.jobid), mimetype=media_type)
-    else:
-        return static_file(rfname, root='{}/{}/results'.format(JOBDATA_PATH, job.jobid), mimetype=media_type, download=rfname)
+    try:
+        user = set_user()
+        # Get job properties from DB
+        job = Job('', jobid, user, get_parameters=True, get_results=True)
+        # Check if result exists
+        if rname not in job.results:
+            raise storage.NotFoundWarning('Result "{}" NOT FOUND for job "{}"'.format(rname, jobid))
+        # Return result
+        # rfname = job.get_result_filename(rname)
+        #response.content_type = 'text/plain; charset=UTF-8'
+        #return str(job.results[result]['url'])
+        media_type = job.results[rname]['mediaType']
+        logger.debug('{} {} {} {} {}'.format(job.jobname, jobid, rname, rfname, media_type))
+        response.set_header('Content-type', media_type)
+        if any(x in media_type for x in ['text', 'xml', 'json']):
+            return static_file(rfname, root='{}/{}/results'.format(JOBDATA_PATH, job.jobid), mimetype=media_type)
+        else:
+            return static_file(rfname, root='{}/{}/results'.format(JOBDATA_PATH, job.jobid), mimetype=media_type, download=rfname)
+    except storage.NotFoundWarning as e:
+        abort_404(e.message)
+    except:
+        abort_500_except()
 
 
 # ----------
