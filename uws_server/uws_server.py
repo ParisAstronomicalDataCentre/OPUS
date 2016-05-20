d584ebb9577b62eb8f6d10c65a171740d8eddb9e
mservillat <mathieu.servillat@obspm.fr>
2016-05-20 12:07:20 +0200

    maintenance code

--- uws_server/uws_server.py
+++ uws_server/uws_server.py
@@ -519,8 +519,9 @@ def maintenance(jobname):
         # TODO: Check consistency of dates (destruction_time > end_time > start_time > creation_time)
         # TODO: Update status if phase is not PENDING or terminal (or for all jobs?)
         joblist = JobList(jobname, user, phase=['QUEUED', 'EXECUTING'])
-        for job in joblist.jobs:
-            phase = job['phase']
+        for j in joblist.jobs:
+            job = Job(jobname, j['jobid'], user, get_attributes=True, get_parameters=False, get_results=False)
+            phase = job.phase
             new_phase = job.get_status()  # will update the phase from manager
             if new_phase != phase:
                 logger.info('{} {} phase {} --> {}'.format(jobname, job.jobid, phase, new_phase))
