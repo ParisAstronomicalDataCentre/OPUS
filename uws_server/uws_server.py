526f37e7ac8dc6f6564385b348b2ab6446acc290
mservillat <mathieu.servillat@obspm.fr>
2016-06-10 15:57:46 +0200

    UWS 1.1: add PHASE kw on get_joblist

--- uws_server/uws_server.py
+++ uws_server/uws_server.py
@@ -643,7 +643,7 @@ def get_joblist(jobname):
             phase = request.query.get('PHASE')
             logger.info({k: request.query[k] for k in request.query})
             logger.info({k: request.GET[k] for k in request.GET})
-            logger.info('{} PHASE={} [{}]'.format(phase, jobname, user))
+            logger.info('{} PHASE={} [{}]'.format(jobname, phase, user))
             joblist = JobList(jobname, user, phase=phase)
         else:
             logger.info('{} [{}]'.format(jobname, user))
