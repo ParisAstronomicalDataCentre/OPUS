12bf5aabebfb0d6cc6aa261e0d223dfaac54c27f
mservillat <mathieu.servillat@obspm.fr>
2017-07-12 22:30:17 +0200

    db/init

--- uws_server/uws_server.py
+++ uws_server/uws_server.py
@@ -309,7 +309,7 @@ def init_db():
     #if not is_localhost():
     #    abort_403()
     try:
-        filename = VAR_PATH + '/db_def/job_database.sqlite'
+        filename = APP_PATH + '/uws_server/job_database.sqlite'
         with open(filename) as f:
             sql = f.read()
         db = storage.__dict__[STORAGE + 'JobStorage']()
@@ -334,7 +334,7 @@ def test_db():
     #if not is_localhost():
     #    abort_403()
     try:
-        filename = VAR_PATH + '/db_def/job_database_test.sqlite'
+        filename = APP_PATH + '/uws_server/job_database_test.sqlite'
         with open(filename) as f:
             sql = f.read()
         db = storage.__dict__[STORAGE + 'JobStorage']()
