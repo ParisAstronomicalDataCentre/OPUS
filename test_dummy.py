036feffb0c1631a961f1af8f16fee5362f4713c3
mservillat <mathieu.servillat@obspm.fr>
2018-04-24 13:09:04 +0200

    run kill test with local manager

--- test_dummy.py
+++ test_dummy.py
@@ -1,6 +1,10 @@
 #!/home/mservillat/anaconda/envs/wsgi/bin/python2.7
 
+import os
+import signal
+import time
 from uws import UWS
+import subprocess as sp
 
 url = "http://localhost/rest/dummy"
 user_name = "anonymous"
@@ -10,18 +14,25 @@ uws_client = UWS.client.Client(url=url)  # , user=user_name, password=password)
 # curl -d "input=Hello World" $URL/rest/dummy
 
 # Create dummy job with default parameters
-job = uws_client.new_job({'other': 'test'})
-job = uws_client.run_job(job.job_id)
-pid = job.job_info[0].text
-print(pid)
+def create_job():
+    job = uws_client.new_job({'run_id': 'test'})
+    job = uws_client.run_job(job.job_id)
+    pid = int(job.job_info[0].text)
+    print('Job created and started: PHASE={}, pid={}'.format(job.phase, pid))
+    return job, pid
 
-# send kill signal to process
+# create job
+job, pid = create_job()
 
-# send hup
+time.sleep(2)
+phase = uws_client.get_phase(job.job_id)
+print('Job phase: {}'.format(phase))
+print('Testing SIGINT')
+os.system("sudo -u _www kill {}".format(pid))
 
-# send quit
+while phase not in ['ERROR', 'COMPLETED']:
+    time.sleep(2)
+    phase = uws_client.get_phase(job.job_id)
+    print('Job phase: {}'.format(phase))
 
-# send term
-
-# send stop
-
+print(uws_client.get_job(job.job_id))
