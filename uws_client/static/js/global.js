7853b298565a74d80bcdf4342b3b6c1c7a11938d
mservillat <mathieu.servillat@obspm.fr>
2018-04-27 14:27:33 +0200

    try fadeOut for messages

---
+++ uws_client/static/js/global.js
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) 2018 by Mathieu Servillat
+ * Licensed under MIT (https://github.com/mservillat/uws-server/blob/master/LICENSE)
+ */
+
+(function($) {
+    "use strict";
+
+    $(document).ready( function() {
+        $('.fadeOut').remove();
+    }
+
+})(jQuery);
