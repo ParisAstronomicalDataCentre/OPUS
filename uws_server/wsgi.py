0bdf5e91cf77e534ad20162fc97cad67cce8c1b2
mservillat <mathieu.servillat@obspm.fr>
2018-05-22 17:36:13 +0200

    remove . in import

--- uws_server/wsgi.py
+++ uws_server/wsgi.py
@@ -9,19 +9,13 @@ WSGI script for UWS server
 import os
 import sys
 
-print(sys.path)
-
 curdir = os.path.dirname(__file__)
 sys.path.append(curdir)
-#sys.path.append(os.path.join(curdir, 'uws_server'))
-#sys.path.append(os.path.join(curdir, 'uws_client'))
-print(sys.path)
 
 # Change working directory so relative paths (and template lookup) work again
 os.chdir(curdir)
 
-import bottle
-from .uws_server import uws_server
+from uws_server import uws_server
 
 # Do NOT use bottle.run() with mod_wsgi
 application = uws_server.app
