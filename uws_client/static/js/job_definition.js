95d164962605e7349f3b07bbdf185c4537b6ca7f
mservillat <mathieu.servillat@obspm.fr>
2018-06-05 14:56:42 +0200

    bs input-group width

--- uws_client/static/js/job_definition.js
+++ uws_client/static/js/job_definition.js
@@ -131,7 +131,7 @@
                                 <input class="used_annotation form-control" name="used_annotation_' + ii + '" type="text" placeholder="Description" style="border-bottom-right-radius: 4px; border-top-right-radius: 4px;" />\
                             </div>\
                             <div style="height: 1px;"></div>\
-                            <div class="input-group input-group-sm col-md-12">\
+                            <div class="input-group input-group-sm col-md-12" style="width:100%">\
                                 <span class="input-group-addon" style="width:70px" title="The input is a File or an ID, possibly with a URL to resolve the ID and download the file (use $ID in the URL template).">\
                                     File <input class="used_isfile" name="used_isfile_' + ii + '" type="radio" value="File" checked/>\
                                     or value  <input class="used_isfile" name="used_isfile_' + ii + '" type="radio" value="value"/>\
@@ -172,7 +172,7 @@
                                 </span>\
                             </div>\
                             <div style="height: 1px;"></div>\
-                            <div class="input-group input-group-sm col-md-12">\
+                            <div class="input-group input-group-sm col-md-12" style="width:100%">\
                                 <span class="input-group-addon" style="width:70px" title="Description">Desc.</span>\
                                 <input class="generated_annotation form-control" name="generated_annotation_' + ii + '" type="text" placeholder="Description" style="border-bottom-right-radius: 4px; border-top-right-radius: 4px;" />\
                             </div>\
