3044de6cb0d5889939fdcb72e669360398ca877a
mservillat <mathieu.servillat@obspm.fr>
2018-07-03 17:03:18 +0200

    add server_accounts page on client

---
+++ uws_client/static/js/server_accounts.js
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) 2018 by Mathieu Servillat
+ * Licensed under MIT (https://github.com/mservillat/uws-server/blob/master/LICENSE)
+ */
+
+(function($) {
+    "use strict";
+
+    var server_url;
+    var endpoint;
+
+    $(document).ready( function() {
+    
+        // Get jobname/jobid
+        server_url = $('#server_url').attr('value');
+        endpoint = $('#endpoint').attr('value');
+        // Get user list
+
+        // Display user list with update button
+
+    });
+
+})(jQuery);
