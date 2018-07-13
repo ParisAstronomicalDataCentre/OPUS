597aa40a5e346ae398a069172c8d852ac5cda71b
mservillat <mathieu.servillat@obspm.fr>
2018-07-13 14:22:32 +0200

    add server_accounts function

--- uws_client/static/js/server_accounts.js
+++ uws_client/static/js/server_accounts.js
@@ -9,14 +9,91 @@
     var server_url;
     var endpoint;
 
+    function get_users() {
+        //var jobname = $('input[name=name]').val();
+        // ajax command to get_users on UWS server
+        $.ajax({
+			url : server_url + '/scim/v2/Users',
+			type : 'GET',
+			dataType: "json",
+			success : function(json) {
+                // Display user list with update button
+                var users = json['Resources'];
+                for (var u in users) {
+                    var user = users[u]
+                    console.log(user.userName);
+                    var row = '\
+            <div id="' + user.userName + '" class="row form-group">\
+                <label class="col-md-2 control-label">' + user.userName + '</label>\
+                <div class="col-md-4 controls ">\
+                    <div class="input-group">\
+                        <input class="form-control" id="roles_' + user.userName + '" name="roles_' + user.userName + '" type="text" value="' + user.roles + '"/>\
+                        <div class="input-group-btn">\
+                            <button type="button" class="start btn btn-default">\
+                                <span class="glyphicon glyphicon-send"></span>\
+                                <span class="hidden-xs hidden-sm hidden-md"></span>\
+                            </button>\
+                        </div>\
+                    </div>\
+                </div>\
+                <div class="col-md-4 controls">\
+                    <div class="input-group">\
+                        <input class="form-control" id="token_' + user.userName + '" name="token_' + user.userName + '" type="text" value="' + user.token + '"/>\
+                        <div class="input-group-btn">\
+                            <button type="button" class="start btn btn-default">\
+                                <span class="glyphicon glyphicon-send"></span>\
+                                <span class="hidden-xs hidden-sm hidden-md"></span>\
+                            </button>\
+                        </div>\
+                    </div>\
+                </div>\
+                <div class="col-md-2">\
+                    <button type="button" class="delete btn btn-default">\
+                        <span class="glyphicon glyphicon-trash"></span>\
+                        <span class="hidden-xs hidden-sm hidden-md">Delete</span>\
+                    </button>\
+                </div>\
+            </div>';
+                    $('#server_accounts_patch').append(row)
+                }
+			},
+			error : function(xhr, status, exception) {
+				console.log(exception);
+				global.showMessage('Cannot get users list from server');
+				// $("#messages").append('<div class="fadeOut alert alert-' + category + ' text-center">' + msg + '</div>').delay(2000).fadeOut();
+			}
+		});
+    }
+
+
+    function patch_user(name, key, value) {
+
+    }
+
+
     $(document).ready( function() {
     
         // Get jobname/jobid
         server_url = $('#server_url').attr('value');
         endpoint = $('#endpoint').attr('value');
+
         // Get user list
+        get_users();
 
         // Display user list with update button
+
+
+        // catch the form's submit event to validate form
+        $('#server_accounts_patch').submit(function(event) {
+            event.preventDefault();
+            var formData = new FormData($('#job_params')[0]);
+//            $('input[type=file]').each( function() {
+//                console.log('Adding: ' + $(this).attr('name'));
+//                console.log($(this)[0].files[0]);
+//                formData.append($(this).attr('name'), $(this)[0].files[0]);
+//            });
+            uws_client.createJob(jobname, formData);
+        });
 
     });
 
--- uws_client/static/js/uws_client.js
+++ uws_client/static/js/uws_client.js
@@ -296,34 +296,34 @@ var uws_client = (function($) {
                 </td>\
                 <td class="text-center" style="vertical-align: middle;">\
                     <div class="btn-group">\
-                    <button type="button" class="properties btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-info-sign"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;Properties</span>\
-                    </button>\
-                    <button type="button" class="parameters btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-edit"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;&nbsp;Parameters</span>\
-                    </button>\
-                    <button type="button" class="results btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-open"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;Results</span>\
-                    </button>\
+                        <button type="button" class="properties btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-info-sign"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;Properties</span>\
+                        </button>\
+                        <button type="button" class="parameters btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-edit"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;&nbsp;Parameters</span>\
+                        </button>\
+                        <button type="button" class="results btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-open"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;Results</span>\
+                        </button>\
                     </div>\
                 </td>\
                 <td class="text-center" style="vertical-align: middle;">\
                     <div class="btn-group">\
-                    <button type="button" class="start btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-play"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;Start</span>\
-                    </button>\
-                    <button type="button" class="abort btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-off"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;Abort</span>\
-                    </button>\
-                    <button type="button" class="delete btn btn-default btn-sm">\
-                        <span class="glyphicon glyphicon-trash"></span>\
-                        <span class="hidden-xs hidden-sm hidden-md">&nbsp;Delete</span>\
-                    </button>\
+                        <button type="button" class="start btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-play"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;Start</span>\
+                        </button>\
+                        <button type="button" class="abort btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-off"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;Abort</span>\
+                        </button>\
+                        <button type="button" class="delete btn btn-default btn-sm">\
+                            <span class="glyphicon glyphicon-trash"></span>\
+                            <span class="hidden-xs hidden-sm hidden-md">&nbsp;Delete</span>\
+                        </button>\
                     </div>\
                 </td>\
             </tr>';
