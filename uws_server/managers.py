45d4289bf918b52b551629ef1d98eaad12fab093
mservillat <mathieu.servillat@obspm.fr>
2018-07-27 16:47:23 +0200

    correct if test

--- uws_server/managers.py
+++ uws_server/managers.py
@@ -122,7 +122,7 @@ class Manager(object):
             rfname = job.get_result_filename(rname)
             line = [
                 '    flist=`ls {rfname} 2>/dev/null`',
-                '    if [ -z "{rfname}"] || [ -z $flist ]; then',
+                '    if ([ -z "{rfname}"] || [ -z $flist ]); then',
                 '        echo "NOT FOUND: {rname}={rfname}"',
                 '    else',
                 '        for fresult in $flist; do',
