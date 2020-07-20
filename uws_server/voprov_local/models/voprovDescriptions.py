38580f14e5fc24fb2a4f99593f472f15eb4835e0
mservillat <mathieu.servillat@obspm.fr>
2020-07-20 17:54:25 +0200

    include local version of voprov for tests

--- uws_server/voprov_local/models/voprovDescriptions.py
+++ uws_server/voprov_local/models/voprovDescriptions.py
@@ -2,8 +2,8 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from .model import (ProvElement, ProvBundle, ProvEntity)
-from voprov.models.constants import *
+from prov.model import (ProvElement, ProvBundle, ProvEntity)
+from .constants import *
 
 __author__ = 'Jean-Francois Sornay'
 __email__ = 'jeanfrancois.sornay@gmail.com'
