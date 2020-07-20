2c868cd6d6d69272dcb447d671e67bd5f8d88bde
mservillat <mathieu.servillat@obspm.fr>
2020-07-20 21:06:55 +0200

    prov import changed

--- uws_server/voprov_local/visualization/dot.py
+++ uws_server/voprov_local/visualization/dot.py
@@ -2,7 +2,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-from prov.dot import GENERIC_NODE_STYLE, DOT_PROV_STYLE, prov_to_dot
+from prov.dot import GENERIC_NODE_STYLE, DOT_PROV_STYLE
 
 from .graph import *
 
